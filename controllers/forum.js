54c5cf9fc13f04355eda455d5806e74c6b19f07c
fengminchao <360322537@qq.com>
2016-12-23 23:22:00 +0800

    + forum controllers

--- controllers/forum.js
+++ controllers/forum.js
@@ -91,25 +91,157 @@ exports.deleteTopic = function(req,res){
 			},
 			function(user,callback){
 				mysqlUtil.query({
-					sql: 'delete * from forum where mail = ?',
+					sql: 'delete * from forum where id = ?',
 					params: [
-
+						req.params.pid
 					]
+				},function(err,rows){
+					callback(err,user);
+				})
+			},
+			function(user,callback){
+				mysqlUtil.query({
+					sql: 'delete * from reply where pid = ?',
+					params: [
+						req.params.pid
+					]
+				},function(err,rows){
+					callback(err,user);
 				})
 			}
-		])
+		],function(err,results){
+			if(handleErr(err,req,res)){
+				return;
+			}
+			res.statusCode = 200;
+			res.send({
+				code: 0,
+				msg: ''
+			})
+		})
 }
 
 exports.newReply = function(req,res){
-	async.water
+	async.waterfall([
+			function(callback){
+				callback(null,req.get('Authorization'));
+			},
+			getUserByToken,
+			function(user,callback){
+				mysqlUtil.query({
+					sql: 'insert into reply (pid,name,avator,content,time,mail) values (?,?,?,?,?,?)',
+					params: [
+						req.params.pid,
+						user.name,
+						user.avator,
+						req.body.content,
+						Date.now(),
+						user.mail
+					]
+				},function(err,rows){
+					callback(err,rows);
+				})
+			},
+			function(rows,callback){
+				mysqlUtil.query({
+					sql: 'update topic set time = ? where id = ?',
+					params: [
+						rows[0].time,
+						rows[0].pid
+					]
+				},function(err,rows){
+					callback(err,rows);
+				})
+			}
+		],function(err,results){
+			if(handleErr(err,req,res)){
+				return;
+			}
+			res.statusCode = 200;
+			res.send({
+				code: 0,
+				msg: ''
+			})
+		})
 }
 
 exports.getTopics = function(req,res){
-	
+	mysqlUtil.query({
+		sql: 'select * from forum order by time'
+	},function(err,rows){
+		if (err) {
+			res.statusCode = 500;
+			return res.send({
+				code: 1,
+				msg: err
+			})
+		}
+		res.statusCode = 200;
+		var postList = [];
+		for (var i = 0; i < rows.length(); i ++) {
+			postList.push(rows[i]);
+		}
+		res.send({
+			code: 0,
+			msg: '',
+			data: postList
+		});
+	})	
 }
 
 exports.getTopic = function(req,res){
-	
+	var topic;
+	var replys = [];
+	async.waterfall([
+			function(callback){
+				callback(null,req.params.pid);
+			},
+			function(pid,callback){
+				mysqlUtil.query({
+					sql: 'select * from forum where id = ?',
+					params: [
+						pid
+					]
+				},function(err,rows){
+					callback(err,rows[0]);
+				})
+			},
+			function(result,callback){
+				result.mail = null;
+				top = result;
+				mysqlUtil.query({
+					sql: 'select * from reply where pid = ?',
+					params: [
+						req.params.pid
+					]
+				},function(err,rows){
+					for(var i = 0;i < rows.length();i ++){
+						rows[i].id = null;
+						rows[i].mail = null;
+						replys.push(rows[i]);
+					}
+					callback(err,rows);
+				})
+			}
+		],function(err,rows){
+			if(err){
+				res.statusCode = 500;
+				return res.send({
+					code: 1,
+					msg: err
+				});
+			}
+
+			res.statusCode = 200;
+			res.send({
+				code: 0,
+				msg: '',
+				data: {
+					topic: topic,
+					replys: replys					
+				}
+			})
+		})
 }
 
 function getUserByToken(token,callback){
